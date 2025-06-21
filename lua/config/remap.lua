2cadcc8f00984576e7b1c2275d4e2dffedd8b73e
jpxx-arts <jpramalho123@hotmail.com>
2025-06-21 18:01:27 -0300

    remap common native operations

--- lua/config/remap.lua
+++ lua/config/remap.lua
@@ -4,3 +4,8 @@ vim.keymap.set('v', '<C-c>', '"+y', { noremap = true, silent = true, desc = 'Yan
 -- Paste from system clipboard
 vim.keymap.set('n', '<C-v>', '"+p', { noremap = true, silent = true, desc = 'Paste from system clipboard' })
 
+-- Key mappings for Neovim
+vim.keymap.set('n', '<leader>pv', vim.cmd.Ex)
+vim.keymap.set('n', '<leader>w', vim.cmd.w)
+vim.keymap.set('n', '<leader>q', function() vim.cmd('q!') end, { noremap = true, silent = true })
+
