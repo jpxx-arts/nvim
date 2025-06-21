70ee1cb2e836a7d703a0e171d633259194c1e2f0
jpxx-arts <jpramalho123@hotmail.com>
2025-06-21 18:16:18 -0300

    add nvim-treesitter plugin

---
+++ lua/plugins/nvim-treesitter.lua
@@ -0,0 +1,22 @@
+return {
+  "nvim-treesitter/nvim-treesitter",
+  build = ":TSUpdate",
+  event = { "BufReadPost", "BufNewFile" },
+  opts = {
+    ensure_installed = {
+      "lua", "vim", "bash", "python", "java", "javascript", "html", "css", "c"
+    },
+    auto_install = true,
+    highlight = {
+      enable = true,
+      additional_vim_regex_highlighting = false,
+    },
+    indent = {
+      enable = true,
+    },
+  },
+  config = function(_, opts)
+    require("nvim-treesitter.configs").setup(opts)
+  end,
+}
+
