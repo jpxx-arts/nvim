45050ec6933a60a8cee432c1f056d313eba81381
jpxx-arts <jpramalho123@hotmail.com>
2025-06-21 18:17:01 -0300

    add lsp plugins

---
+++ lua/plugins/lsp.lua
@@ -0,0 +1,103 @@
+-- ~/.config/nvim/lua/plugins/lsp.lua
+return {
+  {
+    "neovim/nvim-lspconfig",
+    event = { "BufReadPre", "BufNewFile" },
+    dependencies = {
+      "mason-org/mason.nvim",
+      "mason-org/mason-lspconfig.nvim",
+      "hrsh7th/nvim-cmp",
+      "hrsh7th/cmp-nvim-lsp",
+      "windwp/nvim-autopairs",
+    },
+    config = function()
+      -- 🪟 SignColumn sempre visível
+      vim.opt.signcolumn = "yes"
+
+      -- 🧠 Autopairs
+      require("nvim-autopairs").setup()
+
+      -- 🧰 Mason UI
+      require("mason").setup({
+        ui = {
+          icons = {
+            package_installed = "✓",
+            package_pending = "➜",
+            package_uninstalled = "✗",
+          },
+        },
+      })
+
+      -- 🔌 Mason-LSPConfig
+      require("mason-lspconfig").setup({
+        ensure_installed = { "lua_ls", "bashls" },
+        automatic_installation = true,
+        handlers = {
+          function(server_name)
+            require("lspconfig")[server_name].setup({
+              capabilities = capabilities,
+            })
+          end,
+        },
+      })
+
+      -- ⚙️ Capabilities do nvim-cmp
+      local capabilities = vim.tbl_deep_extend(
+        "force",
+        vim.lsp.protocol.make_client_capabilities(),
+        require("cmp_nvim_lsp").default_capabilities()
+      )
+
+      -- 📦 Configuração dos LSPs
+      local lspconfig = require("lspconfig")
+      local servers = { "lua_ls", "bashls", "gleam", "ocamllsp" }
+      for _, lsp in ipairs(servers) do
+        lspconfig[lsp].setup({ capabilities = capabilities })
+      end
+
+      -- ⚡ Keymaps com autocmd
+      vim.api.nvim_create_autocmd("LspAttach", {
+        desc = "LSP actions",
+        callback = function(event)
+          local opts = { buffer = event.buf }
+
+          vim.keymap.set("n", "K", vim.lsp.buf.hover, opts)
+          vim.keymap.set("n", "gd", vim.lsp.buf.definition, opts)
+          vim.keymap.set("n", "gD", vim.lsp.buf.declaration, opts)
+          vim.keymap.set("n", "gi", vim.lsp.buf.implementation, opts)
+          vim.keymap.set("n", "go", vim.lsp.buf.type_definition, opts)
+          vim.keymap.set("n", "gr", vim.lsp.buf.references, opts)
+          vim.keymap.set("n", "gs", vim.lsp.buf.signature_help, opts)
+          vim.keymap.set("n", "<F2>", vim.lsp.buf.rename, opts)
+          vim.keymap.set("n", "<leader>d", vim.diagnostic.open_float, opts)
+          vim.keymap.set({ "n", "x" }, "<F3>", function()
+            vim.lsp.buf.format({ async = true })
+          end, opts)
+          vim.keymap.set("n", "<F4>", vim.lsp.buf.code_action, opts)
+        end,
+      })
+
+      -- 🔠 Configuração do cmp
+      local cmp = require("cmp")
+      cmp.setup({
+        sources = {
+          { name = "nvim_lsp" },
+        },
+        snippet = {
+          expand = function(args)
+            vim.snippet.expand(args.body) -- Neovim 0.10+
+          end,
+        },
+        mapping = {
+          ["<Tab>"] = cmp.mapping.select_next_item(),
+          ["<S-Tab>"] = cmp.mapping.select_prev_item(),
+          ["<CR>"] = cmp.mapping.confirm({ select = true }),
+          ["<C-Space>"] = cmp.mapping.complete(),
+        },
+      })
+    end,
+    keys = {
+      { "<leader>m", "<cmd>Mason<CR>", desc = "Opens Mason" }
+    },
+  },
+}
