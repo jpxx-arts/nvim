6245a199dec08877e5af036bb0712959e3fccb2e
jpxx-arts <jpramalho123@hotmail.com>
2025-06-22 09:42:59 -0300

    fix: annoying errors of auth copilot

--- lua/plugins/copilot.lua
+++ lua/plugins/copilot.lua
@@ -1,20 +1,28 @@
 return {
   "zbirenbaum/copilot.lua",
-  event = "InsertEnter",
+  cmd = "Copilot",
+  -- event = "InsertEnter",
   keys = {
     { "<leader>ce", "<cmd>Copilot enable<CR>", desc = "Enable Copilot" },
     { "<leader>cd", "<cmd>Copilot disable<CR>", desc = "Disable Copilot" },
     { "<leader>cs", "<cmd>Copilot status<CR>", desc = "Copilot Status" },
   },
-  build = ":Copilot auth",
   config = function()
+    local home = vim.fn.expand("~")
+    local has_auth = vim.fn.filereadable(home .. "/.config/github-copilot/apps.json") == 1
+
+    if not has_auth then
+      vim.notify("You must authenticate. Run :Copilot auth", vim.log.levels.WARN)
+      return
+    end
+
     require("copilot").setup({
       suggestion = {
         enabled = true,
         auto_trigger = true,
         debounce = 75,
         keymap = {
-          accept = "<Tab>",
+          accept = "<C-[>",
           next = "<C-j>",
           prev = "<C-k>",
           dismiss = "<C-e>",
