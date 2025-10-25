9e2d8dd0d69005906686619f33a593c7512ae9e2
jpxx-arts <jpramalho123@hotmail.com>
2025-10-25 13:04:50 -0300

    alter: display relative path in filename on lualine

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -3,5 +3,14 @@ return {
   dependencies = { 'nvim-tree/nvim-web-devicons' },
   opts = {
     theme = 'base16',
+    sections = {
+      lualine_c = {
+        {
+          'filename',
+          file_status = true,
+          path = 1 -- relative path display
+        }
+      }
+    }
   }
 }
